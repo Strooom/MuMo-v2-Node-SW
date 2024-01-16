671003ec3fafb1723951d9eb1af71c79a6337777
Pascal Roobrouck <pascal.roobrouck@gmail.com>
2024-01-16 19:22:12 +0100

    add unit test for channel selection

--- test/test_generic_lora_channelcollection/test.cpp
+++ test/test_generic_lora_channelcollection/test.cpp
@@ -15,21 +15,42 @@ void test_initalize() {
     }
     TEST_ASSERT_EQUAL_UINT32(869'525'000U, loRaChannelCollection::rx2Channel.frequency);
 
-    TEST_ASSERT_EQUAL_UINT32(0U, loRaChannelCollection::currentChannelIndex);
+    TEST_ASSERT_EQUAL_UINT32(0U, loRaChannelCollection::getCurrentChannelIndex());
 }
 
 void test_channel_selection() {
+    TEST_ASSERT_EQUAL_UINT32(0U, loRaChannelCollection::getCurrentChannelIndex());
     loRaChannelCollection::selectNextActiveChannelIndex();
-    TEST_ASSERT_EQUAL_UINT32(1U, loRaChannelCollection::currentChannelIndex);
+    TEST_ASSERT_EQUAL_UINT32(1U, loRaChannelCollection::getCurrentChannelIndex());
     loRaChannelCollection::selectNextActiveChannelIndex();
-    TEST_ASSERT_EQUAL_UINT32(2U, loRaChannelCollection::currentChannelIndex);
+    TEST_ASSERT_EQUAL_UINT32(2U, loRaChannelCollection::getCurrentChannelIndex());
     loRaChannelCollection::selectNextActiveChannelIndex();
-    TEST_ASSERT_EQUAL_UINT32(0U, loRaChannelCollection::currentChannelIndex);
+    TEST_ASSERT_EQUAL_UINT32(0U, loRaChannelCollection::getCurrentChannelIndex());
 }
+
+void test_random_channel_selection() {
+uint32_t nmbrOfTimesSelected[loRaChannelCollection::maxNmbrChannels]{}; // initialize all elements to 0
+    for (auto index = 0U; index < 1000U; index++) {
+        loRaChannelCollection::selectRandomChannelIndex();
+        nmbrOfTimesSelected[loRaChannelCollection::getCurrentChannelIndex()]++;
+    }
+        TEST_ASSERT(nmbrOfTimesSelected[0] > 300);
+        TEST_ASSERT(nmbrOfTimesSelected[1] > 300);
+        TEST_ASSERT(nmbrOfTimesSelected[2] > 300);
+        TEST_ASSERT(nmbrOfTimesSelected[3] == 0);
+        TEST_ASSERT(nmbrOfTimesSelected[4] == 0);
+        TEST_ASSERT(nmbrOfTimesSelected[5] == 0);
+        TEST_ASSERT(nmbrOfTimesSelected[6] == 0);
+        TEST_ASSERT(nmbrOfTimesSelected[7] == 0);
+
+
+}
+
 
 int main(int argc, char **argv) {
     UNITY_BEGIN();
     RUN_TEST(test_initalize);
     RUN_TEST(test_channel_selection);
+    RUN_TEST(test_random_channel_selection);
     UNITY_END();
 }
