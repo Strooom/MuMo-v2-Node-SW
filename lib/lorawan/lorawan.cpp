2f6d4228c775ee18eb0b43534ebbf7699c47759c
Pascal Roobrouck <pascal.roobrouck@gmail.com>
2024-01-29 22:12:23 +0100

    2

--- lib/lorawan/lorawan.cpp
+++ lib/lorawan/lorawan.cpp
@@ -1355,7 +1355,7 @@ uint32_t LoRaWAN::Calculate_MIC3(uint8_t *payload, uint32_t payloadLength, aesKe
 
         // Perform AES encryption
         outputBlock.set(outputAsBytes);
-        outputBlock.encrypt(keyIn);        // AES_Encrypt(outputAsBytes, Key);
+        outputBlock.encrypt(networkKey);        // AES_Encrypt(outputAsBytes, Key);
         memcpy(outputAsBytes, outputBlock.asBytes(), 16);
 
         // Copy New_Data to Old_Data
@@ -1386,7 +1386,7 @@ uint32_t LoRaWAN::Calculate_MIC3(uint8_t *payload, uint32_t payloadLength, aesKe
 
         // Perform last AES routine
         // outputBlock.set(outputAsBytes);
-        outputBlock.encrypt(keyIn);        // AES_Encrypt(outputAsBytes, Key);
+        outputBlock.encrypt(networkKey);        // AES_Encrypt(outputAsBytes, Key);
         // memcpy(outputAsBytes, outputBlock.asBytes(), 16);
 
     } else {
