36a531dfb8b66867d06f4e952eba1d3c45a9fafe
Pascal Roobrouck <pascal.roobrouck@gmail.com>
2024-03-27 11:30:58 +0100

    wip

---
+++ lib/general/lptim1.hpp
@@ -0,0 +1,15 @@
+// #############################################################################
+// ### This file is part of the source code for the MuMo project             ###
+// ### https://github.com/Strooom/MuMo-v2-Node-SW                            ###
+// ### Author(s) : Pascal Roobrouck - @strooom                               ###
+// ### License : https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode ###
+// #############################################################################
+
+#pragma once
+#include <stdint.h>
+
+static constexpr uint32_t timerOffset{128U};        // offset to get timing of LPTIM1 accurate, compensating for start/stop overhead
+
+uint32_t ticksFromSeconds(uint32_t seconds) {
+    return (seconds * 4096U) - timerOffset;
+}
---
+++ lib/general/swaplittleandbigendian.hpp
@@ -0,0 +1,15 @@
+// #############################################################################
+// ### This file is part of the source code for the MuMo project             ###
+// ### https://github.com/Strooom/MuMo-v2-Node-SW                            ###
+// ### Author(s) : Pascal Roobrouck - @strooom                               ###
+// ### License : https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode ###
+// #############################################################################
+#pragma once
+#include <stdint.h>
+
+uint32_t swapLittleBigEndian(uint32_t wordIn);
+uint32_t bytesToBigEndianWord(uint8_t byte0, uint8_t byte1, uint8_t byte2, uint8_t byte3);
+uint8_t bigEndianWordToByte0(uint32_t wordIn);
+uint8_t bigEndianWordToByte1(uint32_t wordIn);
+uint8_t bigEndianWordToByte2(uint32_t wordIn);
+uint8_t bigEndianWordToByte3(uint32_t wordIn);
