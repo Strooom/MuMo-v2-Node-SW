107b50e219c30b1c9c4aee9d440a750c54e22d1a
Pascal Roobrouck <pascal.roobrouck@gmail.com>
2025-06-19 22:44:38 +0200

    recoverFrameCountComplete

--- lib/lorawan/framecount.cpp
+++ lib/lorawan/framecount.cpp
@@ -1,63 +1,14 @@
 #include <framecount.hpp>
 
-// frameCount::frameCount() : asUint32(0) {
-//     frameCountAsWord = 0;
-//     syncBytesFromWord();
-// };
-
-// frameCount::frameCount(uint32_t initialValue) : asUint32(initialValue) {
-//     frameCountAsWord = initialValue;
-//     syncBytesFromWord();
-// };
-
-// frameCount& frameCount::operator=(const uint32_t theFrameCount) {
-//     asUint32         = theFrameCount;
-//     frameCountAsWord = theFrameCount;
-//     syncBytesFromWord();
-//     return *this;
-// }
-
-// frameCount& frameCount::operator=(const frameCount& theFrameCount) {
-//     asUint32         = theFrameCount.asUint32;
-//     frameCountAsWord = theFrameCount.frameCountAsWord;
-//     syncBytesFromWord();
-//     return *this;
-// }
-
-// bool frameCount::operator==(const frameCount& theFrameCount) const {
-//     return (asUint32 == theFrameCount.asUint32);
-// }
-
-// bool frameCount::operator!=(const frameCount& theFrameCount) const {
-//     return (asUint32 != theFrameCount.asUint32);
-// }
-
-// bool frameCount::operator>(const frameCount& theFrameCount) const {
-//     return (asUint32 > theFrameCount.asUint32);
-// }
-
-// frameCount& frameCount::operator++(int) {
-//     asUint32++;
-//     frameCountAsWord = asUint32;
-//     syncBytesFromWord();
-//     return *this;
-// }
-
 void frameCount::increment() {
     ++frameCountAsWord;
-    asUint32 = frameCountAsWord;
     syncBytesFromWord();
 }
 
-// uint8_t& frameCount::operator[](int index) {
-//     return asUint8[index];
-// }
-
 void frameCount::guessFromUint16(uint16_t frameCount16Lsb) {
     for (uint32_t index = 0; index < maximumGap; index++) {
-        if (((asUint32 + index) & 0x0000FFFF) == frameCount16Lsb) {
-            asUint32 += index;
-            frameCountAsWord = asUint32;
+        if (((frameCountAsWord + index) & 0x0000FFFF) == frameCount16Lsb) {
+            frameCountAsWord += index;
             syncBytesFromWord();
             return;
         }
@@ -67,10 +18,8 @@ void frameCount::guessFromUint16(uint16_t frameCount16Lsb) {
 void frameCount::setFromByteArray(const uint8_t bytes[lengthInBytes]) {
     (void)memcpy(frameCountAsBytes, bytes, lengthInBytes);
     syncWordFromBytes();
-    asUint32 = frameCountAsWord;
 }
 void frameCount::setFromWord(const uint32_t wordIn) {
     frameCountAsWord = wordIn;
-    asUint32         = wordIn;
     syncBytesFromWord();
 }
--- lib/lorawan/framecount.hpp
+++ lib/lorawan/framecount.hpp
@@ -11,20 +11,7 @@ class frameCount {
   public:
     static constexpr uint32_t lengthInBytes{4};
     static constexpr uint32_t lengthInWords{1};
-    // old
-    // frameCount();
-    // explicit frameCount(uint32_t theFrameCount);
-    //frameCount& operator=(const uint32_t theFrameCount);
-//    frameCount& operator=(const frameCount& theFrameCount);
-    // bool operator==(const frameCount& theFrameCount) const;
-    // bool operator!=(const frameCount& theFrameCount) const;
-    // bool operator>(const frameCount& theFrameCount) const;
-    // frameCount& operator++(int);
     void guessFromUint16(uint16_t frameCount16Lsb);
-    //    uint32_t toUint32() const { return asUint32; };
-    // uint8_t& operator[](int);
-
-    // new
     void setFromByteArray(const uint8_t bytes[lengthInBytes]);
     void setFromWord(const uint32_t wordIn);
 
@@ -37,11 +24,6 @@ class frameCount {
     // private:
 #endif
     static constexpr uint32_t maximumGap{256};        // maximum gap between last (valid) received framecount and next received framecount.
-    // TODO get rid of this punning via union
-    union {
-        uint32_t asUint32;
-        uint8_t asUint8[lengthInBytes];
-    };
     // These are 2 representations of the same frameCount data, in different formats for easy retrieval and conversion. They are synced when setting the frameCount.
     // Example : value 100
     // 1. asWord = 0x64
